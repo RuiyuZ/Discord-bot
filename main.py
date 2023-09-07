14fabd3a57673176da6e22f008d6422f57978696
RuiyuZ <etain42@gmail.com>
2023-09-07 23:56:49 +0000

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#=========== ThePao's Code =======#
 import discord
 from discord.ext import commands
 import os
@@ -20,15 +19,14 @@ game_status = False
 
 @client.event
 async def on_ready():
-  print("We have logged in as {0.user}".format(client))
-  print("TestCommand Ready!")
+  print("Logged in as {0.user}".format(client))
+  print("Server Ready!")
 
 
 @client.event
 async def on_message(message):
   global teamA_active, teamB_active, game_status, vote_status, teamA, teamB, underCover
 
-  ####定义check 函数
   def check_team_choice(m):
     return m.author == message.author and m.channel == message.channel and m.content.lower(
     ) in ['a', 'b']
@@ -37,49 +35,44 @@ async def on_message(message):
     return
 
   if message.content == '开始内战':
-    startGame()
-    await message.channel.send("选择开始组建A队或B队，输入'A'或者'B'")
+    start_game()
+    await message.channel.send("选择开始组建A队或B队，输入 'A' 或者 'B'")
 
   if message.content == "公布内鬼" and teamA_active and teamB_active:
-    await message.channel.send(f"内鬼是{underCover[0]}, {underCover[1]}")
+    await message.channel.send(f"内鬼是 {', '.join(underCover)}")
     return
+
   if message.content == "开始投票" and vote_status:
-    await message.channel.send(f"请输入为A队或B队投票，输入'A'或者'B'")
+    await message.channel.send("请输入为A队或B队投票，输入 'A' 或者 'B'")
     team_choice_response = await client.wait_for('message',
                                                  check=check_team_choice)
     team_choice = team_choice_response.content.lower()
     if team_choice == 'a':
-      voteA_status = True
       await vote(message, team_choice)
     if team_choice == 'b':
-      voteB_status = True
       await vote(message, team_choice)
 
   if game_status and not vote_status:
-    team_choice_response = await client.wait_for('message',
-                                                 check=check_team_choice)
+    team_choice_response = await client.wait_for('message',check=check_team_choice)
     team_choice = team_choice_response.content.lower()
     if team_choice == 'a' and not teamA_active:
       teamA_active = True
-      await listenToTeam(message, team_choice_response, team_choice)
-      return
+      await handle_team_building(message, team_choice_response, team_choice)
     elif team_choice == 'b' and not teamB_active:
       teamB_active = True
-      await listenToTeam(message, team_choice_response, team_choice)
+      await handle_team_building(message, team_choice_response, team_choice)
       vote_status = True
-      return
 
 
 async def vote(message, team_choice):
   global teamA_active, teamB_active, game_status, teamA, teamB
   team = teamA if team_choice == 'a' else teamB
-  result = ', '.join(
-      str(i + 1) + "号:" + x.global_name for i, x in enumerate(team))
+  result = ', '.join(f"{i + 1}号:{x.global_name}" for i, x in enumerate(team))
   await message.channel.send(f"{team_choice.upper()}队人员编号为：{result}")
   await message.channel.send("请开始投票")
 
 
-def startGame():
+def start_game():
   global teamA_active, teamB_active, game_status, vote_status, teamA, teamB, underCover
   print('Start Game!!!!')
   game_status = True
@@ -91,7 +84,7 @@ def startGame():
   underCover = []
 
 
-async def listenToTeam(message, team_choice_response, team_choice):
+async def handle_team_building(message, team_choice_response, team_choice):
   global teamA, teamB
   team = teamA if team_choice == 'a' else teamB
 
@@ -107,29 +100,28 @@ async def listenToTeam(message, team_choice_response, team_choice):
   message = user_response
   if message.mentions:
     mentioned_users = message.mentions
-    for a in mentioned_users:
+    for u in mentioned_users:
       if len(team) < teamSize:
-        team.append(a)
+        team.append(u)
       else:
         await message.channel.send(
             f'{team_choice.upper()}队不能超过{teamSize}人，请重新输入')
-    res = ','.join(x.global_name for x in team)
+
+    res = ', '.join(x.global_name for x in team)
     await message.channel.send(f'{team_choice.upper()}队: {res}')
     choosed_user = random.choice(mentioned_users)
-    print(f'内鬼是：{choosed_user}')
     underCover.append(choosed_user.global_name)
     await message.channel.send(f'内鬼已经选出，请查看Discord私信')
-
     await choosed_user.send('你是内鬼，收到请回复（回复任何字符都可）')
 
-  def check_yes(m):
-    return m.author == choosed_user and len(m.content) != 0 and isinstance(
-        m.channel, discord.DMChannel)
+    def check_yes(m):
+      return m.author == choosed_user and len(m.content) != 0 and isinstance(
+          m.channel, discord.DMChannel)
 
-  user_response = await client.wait_for('message', check=check_yes)
+    user_response = await client.wait_for('message', check=check_yes)
 
-  if len(user_response.content) != 0:
-    await message.channel.send(f'已收到 {team_choice.upper()} 队内鬼的回复')
+    if len(user_response.content) != 0:
+      await message.channel.send(f'已收到 {team_choice.upper()} 队内鬼的回复')
 
 
 client.run(os.getenv('TOKEN'))
