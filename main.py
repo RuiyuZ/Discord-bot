45a51542c82f700b0c895114a616b4222f1f6aae
Ruiyu Zhang <etain42@gmail.com>
2023-09-10 14:23:13 -0700

    cleanup

--- main.py
+++ main.py
@@ -32,25 +32,3 @@ class Bot(commands.Bot):
 load_dotenv()
 bot = Bot()
 bot.run(os.getenv('TOKEN'))
-
-# @bot.event
-# async def on_ready():
-#     global GUILD
-#     GUILD = bot.get_guild(GUILD)
-#     print(f'Logged in as {bot.user.name}')
-#
-#
-# @bot.event
-# async def on_command_error(ctx, error):
-#     if isinstance(error, commands.CommandNotFound):
-#         await ctx.send("Command not found. Use `/bothelp`, `/start`, `/game`, `/botvote` or `/result`")
-#
-#
-#
-# async def main():
-#     await bot.load_extension('startGame')
-#     await bot.start(os.getenv('TOKEN'))
-#
-#
-# if __name__ == '__main__':
-#     asyncio.run(main())
